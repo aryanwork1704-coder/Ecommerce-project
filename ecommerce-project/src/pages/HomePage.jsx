6574d4346f7644232e43c06d276ebd405ffe0bdc
Aryan <aryan175d@gmail.com>
2025-11-11 22:20:51 +0530

    get cart from the backend and diaplay quantity

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -5,16 +5,22 @@ import './HomePage.css';
 
 export function HomePage() {
     const [products , setProducts] = useState([]);
+    const [cart , setCart] = useState([]);
     useEffect(() => {
         axios.get('http://localhost:3000/api/products')
             .then((response) => {
             setProducts(response.data);
         })
+
+        axios.get('http://localhost:3000/api/cart-items')
+            .then((response) => {
+                setCart(response.data);
+            })
     }, [])
     return (
         <>
             <title>Ecommerce-project</title>
-            <Header />
+            <Header cart={cart} />
 
 
             <div className="home-page">
